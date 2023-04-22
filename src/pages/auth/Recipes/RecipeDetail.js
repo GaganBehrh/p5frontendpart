d0238fcd7506f713ebae076f32cd8549d99307a9
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-04-22 20:41:54 +0000

    Fetching of the data for the post was succesful

--- src/pages/auth/Recipes/RecipeDetail.js
+++ src/pages/auth/Recipes/RecipeDetail.js
@@ -1,13 +1,32 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import { useParams } from "react-router";
+import { axiosReq, axiosRes } from "../../../api/axioDefaults";
 
 
 
 function RecipeDtetail() {
-  // Add your logic here
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
 
   return (
