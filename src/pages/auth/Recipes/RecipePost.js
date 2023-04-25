f125b51b3940aae59d1f2c8c954a5bc4dfd70a44
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-04-25 11:55:58 +0000

    Currentuser issue fixed contents not being displayed

--- src/pages/auth/Recipes/RecipePost.js
+++ src/pages/auth/Recipes/RecipePost.js
@@ -28,13 +28,11 @@ const Post = (props) => {
 
 export default Post;*/
 import React from "react";
-
-
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import LoggedinPic from "../../../components/LoggedinPic";
-import useCurrentUser from "../../contexts/CurrentUser";
+import { useCurrentUser } from "../../../contexts/CurrentUser";
 
 
 const Post = (props) => {
@@ -43,14 +41,7 @@ const Post = (props) => {
         owner,
         profile_id,
         profile_image,
-        comments_count,
-        likes_count,
-        like_id,
-        title,
-        content,
-        image,
-        updated_at,
-        postPage,
+        username
     } = props;
 
     const currentUser = useCurrentUser();
@@ -65,46 +56,21 @@ const Post = (props) => {
                         {owner}
                     </Link>
                     <div className="d-flex align-items-center">
-                        <span>{updated_at}</span>
-                        {is_owner && postPage && "..."}
+
                     </div>
                 </Media>
             </Card.Body>
             <Link to={`/posts/${id}`}>
-                <Card.Img src={image} alt={title} />
+
             </Link>
             <Card.Body>
-                {title && <Card.Title className="text-center">{title}</Card.Title>}
-                {content && <Card.Text>{content}</Card.Text>}
+
                 <div >
-                    {is_owner ? (
-                        <OverlayTrigger
-                            placement="top"
-                            overlay={<Tooltip>You can't like your own post!</Tooltip>}
-                        >
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    ) : like_id ? (
-                        <span onClick={() => { }}>
-                            <i className={`fas fa-heart `} />
-                        </span>
-                    ) : currentUser ? (
-                        <span onClick={() => { }}>
-                            <i className={`far fa-heart`} />
-                        </span>
-                    ) : (
-                        <OverlayTrigger
-                            placement="top"
-                            overlay={<Tooltip>Log in to like posts!</Tooltip>}
-                        >
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    )}
-                    {likes_count}
+
                     <Link to={`/posts/${id}`}>
                         <i className="far fa-comments" />
                     </Link>
-                    {comments_count}
+
                 </div>
             </Card.Body>
         </Card>
