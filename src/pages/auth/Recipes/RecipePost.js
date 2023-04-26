20d443bc857ac8c12800363d13a01199839b024e
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-04-26 08:28:03 +0000

    Post Part of the RecipePost page completed data receivable and completed

--- src/pages/auth/Recipes/RecipePost.js
+++ src/pages/auth/Recipes/RecipePost.js
@@ -2,15 +2,21 @@ import React from "react";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import RecipeDetail from "./RecipeDetail";
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import LoggedinPic from "../../../components/LoggedinPic";
 import { useCurrentUser } from "../../../contexts/CurrentUser";
+import { useHistory } from "react-router";
 
 
 const Post = (props) => {
     let {
         id,
         name,
+        matter,
         owner,
         profile_id,
         profile_image,
@@ -20,58 +26,47 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
-    const buttonFields = (
+    const history = useHistory();
+    const FormFields = (
         <div className="text-center">
             <Form.Group>
+                <Form.Label>UserName</Form.Label>
+                <Form.Control type="text" col={6} name="name" value={owner} />
+            </Form.Group>
+            <Form.Group>
                 <Form.Label>RecipeName</Form.Label>
-                <Form.Control type="text" placeholder="Please enter the name of the recipe" col={6} name="name" value={name} onChange={handleChange} />
+                <Form.Control type="text" col={6} name="name" value={name} />
             </Form.Group>
             <Form.Group>
                 <Form.Label>RecipeSteps</Form.Label>
-                <Form.Control as="textarea" placeholder="Please enter the recipe steps" rows={8} name="matter" value={matter} onChange={handleChange} />
+                <Form.Control as="textarea" placeholder="Please enter the recipe steps" rows={8} value={matter} />
             </Form.Group>
-            <Button variant="outline-success" type="submit">Create Recipe</Button>{' '}
             <Button variant="outline-success" onClick={() => history.goBack()}>Cancel</Button>{' '}
+
         </div>
 
     );
-
     return (
-        
+
         <Card >
             <Card.Body>
-                
+
                 <Media className="align-items-center justify-content-between">
-                    <Link to={`/profiles/${profile_id}`}>
-                        <LoggedinPic src={profile_image} height={55} />
-                        {owner}
-                    </Link>
-                    <div className="d-flex align-items-center">
-                    
-        {name}
-        {owner}
-        {profile_id}
-       
-       {username}
-                    </div>
-                    </Media>
+                    <form>
+                        <Row>
+                            <Col md={5} lg={12} className="d-none d-md-block p-0 p-md-2">
+                                <Container >{FormFields}</Container>
+                            </Col>
+                        </Row>
+                    </form>
+                </Media>
             </Card.Body>
-                    <Row>
-                <Col md={5} lg={12} className="d-none d-md-block p-0 p-md-2">
-                    <Container >{buttonFields}</Container>
-             </Row>
+
             <Link to={`/posts/${id}`}>
 
             </Link>
             <Card.Body>
 
-                <div >
-
-                    <Link to={`/posts/${id}`}>
-                        <i className="far fa-comments" />
-                    </Link>
-
-                </div>
             </Card.Body>
         </Card>
     );
