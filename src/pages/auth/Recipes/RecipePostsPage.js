df6e6d46ebd735c08460fb1f50233696ab2b5186
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-05-19 10:27:08 +0000

    RecipePost page is receiving the data from the backend

--- src/pages/auth/Recipes/RecipePostsPage.js
+++ src/pages/auth/Recipes/RecipePostsPage.js
@@ -11,47 +11,51 @@ import { useLocation } from "react-router";
 import appStyles from "../../../App.module.css";
 import NoResults from "../../../media/no-results.png";;
 
-function RecipePostsPage(message, filter = "") {
+function RecipePostsPage(message) {
   const [posts, setPosts] = useState({ results: [] });
+  console.log(posts);
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/Recipeposts/?${filter}`);
+        const { data } = await axiosReq.get(`/Recipeposts/`);
+        console.log(data);
         setPosts(data);
+        console.log(posts);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
-
+    
     setHasLoaded(false);
     fetchPosts();
-  }, [filter, pathname]);
+  }, [pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        {hasLoaded ? (
+        { hasLoaded ? (
           <>
-            {posts.results.length ? (
+            {posts.results?.length? (
               posts.results.map((post) => (
                 <RecipePostTrial key={post.id} {...post} setPosts={setPosts} />
+
               ))
             ) : (
-              <Container className={appStyles.Content}>
-                <Asset src={NoResults} message={message} />
-              </Container>
+            <Container className={appStyles.Content}>
+              <Asset src={NoResults} message={message} />
+            </Container>
             )}
           </>
         ) : (
           <Container className={appStyles.Content}>
             <Asset spinner />
           </Container>
-        )}
+        )} 
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
