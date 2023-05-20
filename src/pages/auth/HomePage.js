0836d456691006bcbe42a7d522f1923dde8db643
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-05-20 07:15:24 +0000

    Home page completed and functional

--- src/pages/auth/HomePage.js
+++ src/pages/auth/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 import styles from "../../styles/HomePage.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
@@ -32,59 +32,50 @@ const HomePage = () => {
 
   const history = useHistory();
 
-  const handleChange = (event) => {
-    setSignInData({
-      ...SignInData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await axios.post("/dj-rest-auth/login/", SignInData);
-      /*Theis setcurrentuser can break the signin page*/
-   
-      console.log(data.user);
-      console.log("call");
-
-      history.push("/Recipeposts/create");
-    } catch (err) {
-      setErrors(err.response?.data);
-    }
-  };
-
-
   return (
     <Row className={styles.Row}>
        <Col className="my-auto py-2 p-md-2" md={4}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <p className={styles.Header}>Create your own new exciting recipes here
+          <p className={styles.Header} >Create new recipes by simple Sign up 
          </p>
          <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg"}alt="recipe image"
+          src={"https://images-prod.healthline.com/hlcmsresource/images/AN_images/raw-vegan-diet-1296x728-feature.jpg"}alt="recipe image"
         />
 |</Container>
       </Col>
       <Col className="my-auto py-2 p-md-2" md={4}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Donot have an account yet? Sign up here</h1>
+          <h1 className={styles.Header}>Donot have an account yet?
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/signup"
+        >
+            <i className=""></i>Sign up here
+        </NavLink> </h1>
           <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg"}alt="recipe image"
+          src={"https://t4.ftcdn.net/jpg/04/09/12/37/360_F_409123771_SSIjSUiM9AXVyEUVfxDT2zmoKmJDQALi.jpg"}alt="signup image"
         />
 |</Container>
       </Col>
       <Col className="my-auto py-2 p-md-2" md={4}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Already have an account here? Signin here</h1>
+          <h1 className={styles.Header}>Already have an account here?   <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/signin"
+        >
+            <i className=""></i>Login here
+        </NavLink> </h1>
           <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://www.realsimple.com/thmb/2ixrIIlE-44L1MypGHIs2TqIQYk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-recipes-1672e2b4f9104ed3b3867a2a14889ce9.jpg"}alt="recipe image"
+          src={"https://media.istockphoto.com/id/1224417416/vector/login-button-login-square-3d-push-button.jpg?s=612x612&w=0&k=20&c=C6Hyr-sE7JOXUgwhpsSWFHNawidhY4o26wjv63UlXiU="}alt="Login image"
         />
 |</Container>
       </Col>
+     
     </Row>
   );
 };
