7c3111b6f723ed4fcdd8d8aed9f11340cdb0e937
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-05-24 12:58:31 +0000

    Profile data starts to be visible on the front end

--- src/pages/auth/Profiles/ShowProfiles.js
+++ src/pages/auth/Profiles/ShowProfiles.js
@@ -1,25 +1,17 @@
 import axios from "axios";
 import React,{useState, useEffect} from "react";
 import { axiosReq } from "../../../api/axioDefaults";
+import { Card, Button } from 'react-bootstrap';
 const ShowProfiles = () => {
-  const [profiles,setProfiles]=useState({});
-
- /* const getProfiles= async()=>{
-    const response=await axiosReq.get('/Recipeposts/');
-    console.log(response.data);
-  }
-
-  useEffect(()=>{
-    getProfiles();
-
-  },[])*/
+  const [profiles,setProfiles]=useState([]);
 
   useEffect(() => {
     const getProfiles = async () => {
         try {
             const { data } = await axiosReq.get(`/profiles/`);
             console.log(data);
-            
+            setProfiles(data);
+               
 
         } catch (err) {
             console.log(err);
@@ -29,9 +21,33 @@ const ShowProfiles = () => {
   }, []);
 
   return (
-    <div>fgkflgk</div>
-  );
+    <div>
 
+        <div >
+        {
+            profiles.map((profile, index) => (
+                <Card className="m-3 rounded shadow-lg main-students-show" style={{ width: '22em' }}>
+
+                <Card.Img src={profile.image}  width={171}
+        height={180} />
+
+                <Card.Body>
+
+                    <Card.Title>Id:{profile.id}</Card.Title>
+                    <Card.Text> {profile.owner} </Card.Text>
+                  
+
+                </Card.Body>
+                </Card>
+            ))
+
+        }
+        </div>
+       
+        
+    </div>
+);
 };
+     
 
 export default ShowProfiles;
